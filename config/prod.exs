d4c9acd91c1bd9a98801bdf62ec4206294e4a353
csaldiasdev <cristian.saldias.90@outlook.com>
2023-04-14 02:47:51 -0400

    fixes...

--- config/prod.exs
+++ config/prod.exs
@@ -4,3 +4,4 @@ config :scl_bus_stop_info, SclBusStopInfo.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  check_origin: ["https://" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"]
