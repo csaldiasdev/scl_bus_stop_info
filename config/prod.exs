516a1d28859be3837c91e51b366f25798753858a
csaldiasdev <cristian.saldias.90@outlook.com>
2023-04-14 03:29:41 -0400

    si no gg nomas

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 import Config
 
-config :scl_bus_stop_info,
+config :scl_bus_stop_info, SclBusStopInfo.Router.Main,
+  server: true,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
   check_origin: ["https://" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"]
