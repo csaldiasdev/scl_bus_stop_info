93cb09cdeac5806f1f03b7af79848f3eb3f132eb
csaldiasdev <cristian.saldias.90@outlook.com>
2023-04-14 11:56:01 -0400

    vamo que se puede

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,9 @@ import Config
 
 config :scl_bus_stop_info, SclBusStopInfo.Router.Main,
   server: true,
-  http: [port: {:system, "PORT"}],
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
   check_origin: ["https://" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"]
