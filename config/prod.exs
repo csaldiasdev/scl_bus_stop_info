631bd390b94d8a619d5cc4428db827dccad918d6
csaldiasdev <cristian.saldias.90@outlook.com>
2023-04-14 03:20:49 -0400

    last test

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,6 @@
 import Config
 
-config :scl_bus_stop_info, SclBusStopInfo.Endpoint,
-  server: true,
+config :scl_bus_stop_info,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
   check_origin: ["https://" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"]
